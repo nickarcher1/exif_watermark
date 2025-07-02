3968a32006d3dac3e89e03d39f65a503a950d3f9
archenrpi <archen@rpi.edu>
2025-07-01 21:45:21 -0400

    Major updates and crash prevention

--- exif_watermark.py
+++ exif_watermark.py
@@ -74,37 +74,82 @@ class location:
         return lat, lon
 
     def getCityState(self) -> str:
-        try:
+        if 'city' in self.location.keys():
             city = self.location['city']
-        except:
+        elif 'municipality' in self.location.keys():
             city = self.location['municipality']
-        return f"{city}, {self.location['state']}"
+        elif 'village' in self.location.keys():
+            city = self.location['village']
+        elif 'county' in self.location.keys():
+            city = self.location['county']
+        elif 'town' in self.location.keys():
+            city = self.location['town']
+        else:
+            city = ''
+
+        if 'state' in self.location.keys():
+            state = self.location['state']
+        elif 'province'  in self.location.keys():
+            state = self.location['province']
+        elif 'region'  in self.location.keys():
+            state = self.location['region']
+        else:
+            state = ''
+
+        citystate = city
+        if citystate == '':
+            citystate = state
+        else:
+            citystate += f", {state}"
+
+        return citystate
     
     def getCountry(self) -> str:
-        return self.location['country']
+        country = self.location['country']
+        if country == 'Schweiz/Suisse/Svizzera/Svizra':
+            country = 'Schweiz'
+        return country
     
     def __str__(self) -> str:
         return f"{self.getCityState()}\n{self.getCountry()}"
         
-# def getImageData(img: Image.Image) -> dict[str, bool|time|location]:
-def getImageData(img: Image.Image) -> dict:
+def getImageData(img: Image.Image, origin: str) -> dict:
     exif = {
         ExifTags.TAGS[k]: v
         for k, v in img._getexif().items()
         if k in ExifTags.TAGS
     }
-    # out: dict[str, bool|time|location] = {}
     out: dict = {}
-    if exif['Orientation'] == 6:
-        out['turn'] = True
-    else:
-        out['turn'] = False
-    out['timestamp'] = time(exif['DateTime'])
-    out['location'] = location(exif['GPSInfo'])
+    out['turn'] = False
+    if 'Orientation' in exif.keys():
+        if exif['Orientation'] == 6:
+            out['turn'] = True
+    if 'DateTime' in exif.keys():
+        out['timestamp'] = time(exif['DateTime'])
+    if 'GPSInfo' in exif.keys():
+        if len(exif['GPSInfo'].keys()) > 1:
+            out['location'] = location(exif['GPSInfo'])
     return out
 
-def addStampsToPhoto(img: Image.Image, t: time, l: location, rotate: bool):
-    watermark = f"{t}\n{l}"
+def addStampsToPhoto(img: Image.Image, dat: dict) -> tuple[bool, bool]:
+    watermark = ''
+    if 'timestamp' in dat.keys():
+        t = True
+        watermark = str(dat['timestamp'])
+    else:
+        t = False
+    if 'location' in dat.keys():
+        l = True
+        if watermark != '':
+            watermark += '\n'
+        watermark += str(dat['location'])
+    else:
+        l = False
+    rotate = dat['turn']
+
+    if watermark == '':
+        return False, False
+
     draw = ImageDraw.Draw(img)
     if rotate:
         ImageOps.exif_transpose(img)
@@ -118,48 +163,86 @@ def addStampsToPhoto(img: Image.Image, t: time, l: location, rotate: bool):
     y_position = image_height - text_height - margin
 
     draw.text((x_position, y_position), watermark, fill=(255, 255, 255), font=font) # White color for the timestamp
+    return t, l
 
 def stamp_jpg(path: str, out_folder_path: str) -> bool:
     newpath = f"{out_folder_path}{os.sep}{os.path.basename(path)}"
 
     init_img = Image.open(path)
-    exif_bytes = init_img.info.get('exif')
+    exif_bytes = init_img.info.get('exif')                                  # Pull exif data
 
-    if len(init_img.getexif()) == 0:
-        init_img.save(newpath) # , exif = exif_bytes
-        return False
+    if len(init_img.getexif()) == 0:                                        # Don't do anything if inputed photo has no metadata
+        return False, False
     
-    img = ImageOps.exif_transpose(init_img)
-    dat = getImageData(init_img)
-    addStampsToPhoto(img, dat['timestamp'], dat['location'], dat['turn'])
+    img = ImageOps.exif_transpose(init_img)                                 # Will transpose if needed
 
-    exif_dict = piexif.load(exif_bytes)
+    dat = getImageData(init_img, path)                                            # pulls exif time, location, and rotation data
+    if 'timestamp' not in dat.keys() and 'location' not in dat.keys():      # Don't do anything if inputed photo has no time or location metadata
+        return False, False
+    hastime, hasloc = addStampsToPhoto(img, dat)
+    if not hastime and not hasloc:   # Run stamping script
+        return False, False
+
+    exif_dict = piexif.load(exif_bytes)                                     # Load exif data
     if piexif.ImageIFD.Orientation in exif_dict["0th"]:
-        exif_dict["0th"][piexif.ImageIFD.Orientation] = 1
-    exif_bytes = piexif.dump(exif_dict)
+        exif_dict["0th"][piexif.ImageIFD.Orientation] = 1                   # Change orientation to be known as 'stored correctly'
+    exif_bytes = piexif.dump(exif_dict)                                     # Reload edited exif data
 
-    img.save(newpath, exif=exif_bytes) # , exif = exif_bytes
-    return True
+    img.save(newpath, exif=exif_bytes)                                      # Save file, with edited exif data
+    return hastime, hasloc
+
+def manual_mode(files: list[str], missing_time: list[str], missing_loc: list[str]):
+    if len(files) != 0:
+        print("\nThe following files could not be stamped: ")
+        for path in files:
+            print(path)
+    if len(missing_time) != 0:
+        print("\nThe following files are missing their time, but a photo with stamped location has been created: ")
+        for path in missing_time:
+            print(path)
+    if len(missing_loc) != 0:
+        print("\nThe following files are missing their location, but a photo with stamped time has been created: ")
+        for path in missing_loc:
+            print(path)
+    
+
+    
 
 def stamp_all_photos_in_folder(folder_path: str):
+    non_stamped_files = []
+    missing_time = []
+    missing_loc = []
     out_folder = f"{folder_path}{os.sep}WATERMARKED"
     file_paths = []
-    for root, _, files in os.walk(folder_path):
-        for file in files:
-            file_paths.append(os.path.join(root, file))
+    for file in os.listdir(folder_path):
+        filepath = os.path.join(folder_path, file)
+        if os.path.isfile(filepath):
+            file_paths.append(filepath)
     if not os.path.isdir(out_folder):
         try:
             os.makedirs(out_folder, exist_ok = True)
         except OSError as e:
             print(f"Error creating folder '{out_folder}': {e}")
             sys.exit(1)
+    num_completed = 0
+    num_review = 0
+    total = len(file_paths)
     for file in file_paths:
-        if 'WATERMARKED' in file:
-            continue
-        if stamp_jpg(file, out_folder):
-            print(f"SUCCESSFUL: {file}")
+        hastime, hasloc = stamp_jpg(file, out_folder)
+        if hastime and hasloc:
+            num_completed += 1
+        elif not hastime and not hasloc:
+            non_stamped_files.append(file)
+            num_review += 1
         else:
-            print(f"  FAILED  : {file}")
+            if not hastime:
+                missing_time.append(file)
+                num_review += 1
+            if not hasloc:
+                missing_loc.append(file)
+                num_review += 1
+        print(f"\rCompleted: {num_completed} of {total}. Num to review: {num_review} of {total}. Total processed: {num_completed + num_review} of {total}.", end="")
+    manual_mode(non_stamped_files, missing_time, missing_loc)
 
 def main():
     if len(sys.argv) != 2:
