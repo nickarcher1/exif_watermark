69efd17b66446b44a7941476cef59817eb1684e6
archenrpi <archen@rpi.edu>
2025-07-01 22:24:49 -0400

    manual mode wip

--- dependencies/manualediting_functions.py
+++ dependencies/manualediting_functions.py
@@ -1,12 +1,4 @@
-def getYesNo(question_text: str) -> bool:
-    answer = 'q'
-    while(answer not in ['y', 'n', 'yes', 'no']):
-        answer = input(f"{question_text} (y/n) ").lower()
-    if answer in ['y', 'yes']:
-        return True
-    return False
-
-def manual_mode(files: list[str], missing_time: list[str], missing_loc: list[str]):
+def print_completion_summary(files: list[str], missing_time: list[str], missing_loc: list[str]):
     if len(files) != 0:
         print("\nThe following files could not be stamped: ")
         for path in files:
@@ -19,7 +11,15 @@ def manual_mode(files: list[str], missing_time: list[str], missing_loc: list[str
         print("\nThe following files are missing their location, but a photo with stamped time has been created: ")
         for path in missing_loc:
             print(path)
-    resolve = getYesNo("Would you like to resolve the missing stamps?")
-    print(resolve)
-    if not resolve:
-        return
+
+def getYesNo(question_text: str) -> bool:
+    answer = 'q'
+    while(answer not in ['y', 'n', 'yes', 'no']):
+        answer = input(f"{question_text} (y/n) ").lower()
+    if answer in ['y', 'yes']:
+        return True
+    return False
+
+def manual_mode(files: list[str], missing_time: list[str], missing_loc: list[str]):
+    print("MANUAL FIX is WIP")
+    
--- exif_watermark.py
+++ exif_watermark.py
@@ -4,21 +4,27 @@ pip dependencies:
     - geopy
     - piexif
 file/folder dependencies:
-    - dependencies\stamping_functions
-    - dependencies\metadata_classes
-    - dependencies\manualediting_functions
+    - dependencies/stamping_functions
+    - dependencies/metadata_classes
+    - dependencies/manualediting_functions
     - folder path supplied as cmd argument
 """
 import sys
 from dependencies.stamping_functions import stamp_all_photos_in_folder
-from dependencies.manualediting_functions import manual_mode
+from dependencies.manualediting_functions import manual_mode, print_completion_summary, getYesNo
 
 def main():
     if len(sys.argv) != 2:
         print("ERROR: too many or too few cmd arguments.")
         return
+    
     non_stamped_files, missing_time, missing_loc = stamp_all_photos_in_folder(sys.argv[1])
-    manual_mode(non_stamped_files, missing_time, missing_loc)
+
+    print_completion_summary(non_stamped_files, missing_time, missing_loc)
+
+    resolve = getYesNo("Would you like to resolve the missing stamps?")
+    if resolve:
+        manual_mode(non_stamped_files, missing_time, missing_loc)
 
 if __name__ == "__main__":
     main()
